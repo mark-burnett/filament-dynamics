312d4eeb2986046418752c64c3bb7249d002aa2a
mark.m.burnett <devnull@localhost>
2009-11-06 19:22:21 +0000

    Added proper copyright notices for the GPL. Added a copy of the GPL text.
    Added ppmap, which is a parallel processing replacement for the python builtin
    map function.  After some basic testing, it seems much more reliable and
    scalable than forkmap (though it does have a few quirks).

--- diffusion.py
+++ diffusion.py
@@ -1,10 +1,29 @@
+#    Copyright (C) 2009 Mark Burnett
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from numpy import array, average, linspace, roll
 from scipy.stats import linregress
 
-def _dispersion( L, window_size, step_size ):
+def _dispersion( L, window_size ):
     shifted = roll( L, -window_size )
     difference = L[:-window_size] - shifted[:-window_size]
-    return average( difference**2 )
+    # NOTE: perhaps this would be more accurate as
+    #  avg( diff**2 ) - avg( diff )**2
+    # but it only seems to be a 1/100 of a percent difference
+#    return average( difference**2 )
+    return average( difference**2 ) - average( difference )**2
 
 def _fluctuations( L, time, dt, n_pts ):
     """
@@ -15,7 +34,7 @@ def _fluctuations( L, time, dt, n_pts ):
     values = []
     t_win_sizes = linspace( 100 * dt, time[-1]/2, n_pts )
     window_sizes = [ int(t/dt) for t in t_win_sizes ]
-    values = [ _dispersion( array(L), ws, 1 ) for ws in window_sizes ]
+    values = [ _dispersion( array(L), ws ) for ws in window_sizes ]
 
     return linregress( 2*t_win_sizes, values )
 
