c4ef1591cfbaa42eef2184e1ebc4d1ab132f79b4
mark.m.burnett@gmail.com <mark.m.burnett@gmail.com>
2011-02-18 11:45:03 -0600

    Basic analysis strategy (very simple and naive). YAML definition loader (with import support) drafted.

---
+++ actin_dynamics/analysis_strategy.py
@@ -0,0 +1,25 @@
+#    Copyright (C) 2011 Mark Burnett
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+class Analysis(object):
+    def __init__(self, label, elements):
+        self.label = label
+        self.elements = elements
+
+    def perform(self, run_results):
+        previous_result = run_results
+        for element in elements:
+            previous_result = element.process(previous_result)
+        return previous_result
---
+++ actin_dynamics/io/definitions.py
@@ -0,0 +1,43 @@
+#    Copyright (C) 2011 Mark Burnett
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import yaml
+import os.path
+
+def load_definition(filename, source_directory='configuration'):
+    results = None
+    with open(os.path.join(source_directory, filename)) as f:
+        results = yaml.load(f)
+
+    imports = result.get('import', [])
+    for import_filename in imports:
+        results = merge_dicts(results,
+                load_session_definition(import_filename, source_directory))
+
+    return results
+
+def merge_dicts(a, b):
+    keys = set(a.keys()).union(set(b.keys()))
+
+    result = {}
+    for key in keys:
+        if key not in a:
+            result[key] = b[key]
+        elif key not in b:
+            result[key] = a[key]
+        else: # key in both
+            result[key] = _merge_dicts(a[key], b[key])
+
+    return result
