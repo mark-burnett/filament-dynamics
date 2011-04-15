82d0fdf7ed695b435774fa1366c5f2b067ad12e8
Mark Burnett <mark.m.burnett@gmail.com>
2011-04-15 01:25:36 -0500

    Added rate_bisect.

---
+++ actin_dynamics/numerical/rate_bisect.py
@@ -0,0 +1,26 @@
+#    Copyright (C) 2011 Mark Burnett
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import bisect
+
+def rate_bisect(r, right_edges):
+    index = bisect.bisect_right(right_edges, r)
+
+    if index:
+        remaining_r = right_edges[index - 1] - r
+    else:
+        remaining_r = r
+
+    return index, remaining_r
