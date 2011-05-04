f892b5de46243c33ceec9f843d8392b9ab8b5f9e
Mark Burnett <mark.m.burnett@gmail.com>
2011-05-03 20:18:59 -0500

    Removed unused import.

--- actin_dynamics/configuration/database.py
+++ actin_dynamics/configuration/database.py
@@ -13,8 +13,6 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import configobj
-
 from sqlalchemy import orm, create_engine
 
 from actin_dynamics import database
