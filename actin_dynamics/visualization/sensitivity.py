6865c6efcaa6c8af1921eb605773394d72c18e5a
Mark Burnett <mark.m.burnett@gmail.com>
2011-10-26 13:05:45 -0500

    Some polish on the sensitivity.

--- actin_dynamics/visualization/sensitivity.py
+++ actin_dynamics/visualization/sensitivity.py
@@ -13,22 +13,48 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import csv
+
 from actin_dynamics import database
+from actin_dynamics.io import data
 
-def calculate(session_id, parameter_name='release_rate',
-        objective_name='halftime'):
+def save(session_ids, output_filename='results/sensitivities.dat'):
     dbs = database.DBSession()
-    session = dbs.query(database.Session).get(session_id)
+    rows = []
+    for session_id in session_ids:
+        session = dbs.query(database.Session).get(session_id)
+        cooperativity = session.parameters['release_cooperativity']
+        rate, halftime, halftime_error, low_sens, high_sens, sensitivity = calculate(session)
 
+        halftime_fit_error = abs(halftime - 388)
+        total_halftime_error = halftime_error + halftime_fit_error
+
+        total_rate_error = total_halftime_error / sensitivity
+
+        rows.append([cooperativity, rate, total_rate_error,
+            halftime, halftime_error, low_sens, high_sens, sensitivity])
+
+    _small_writer(output_filename, rows,
+            ['release_cooperativity', 'release_rate', 'release rate error',
+                'halftime', 'halftime error',
+                'low sens', 'high sens' 'average sensitivity'])
+
+
+
+def calculate(session, parameter_name='release_rate',
+        objective_name='halftime', error_name='halftime_error'):
     runs = session.experiments[0].runs
 
     us_parameters = [r.parameters[parameter_name] for r in runs]
     us_objectives = [r.get_objective(objective_name) for r in runs]
+    us_errors = [r.get_error(error_name) for r in runs]
 
-    parameters, objectives = zip(*sorted(zip(us_parameters, us_objectives)))
+    parameters, objectives, errors = zip(*sorted(
+        zip(us_parameters, us_objectives, us_errors)))
 
     base_par = parameters[1]
     base_obj = objectives[1]
+    base_error = errors[1]
 
     low_par = (parameters[0] - base_par)
     high_par = (parameters[2] - base_par)
@@ -39,4 +65,17 @@ def calculate(session_id, parameter_name='release_rate',
     low_sense = low_obj / low_par
     high_sense = high_obj / high_par
 
-    return low_sense, high_sense
+    return base_par, base_obj, base_error, low_sense, high_sense, (low_sense + high_sense) / 2
+
+
+def _small_writer(filename, results, names, header=None):
+    with open(filename, 'w') as f:
+        # Header lines, identifying x, y, column name
+        f.write('# Auto-collated output:\n')
+        if header:
+            f.write(header)
+        for i, name in enumerate(names):
+            f.write('# Column %i: %s\n' % ((i + 1), name))
+        # CSV dump of actual data
+        w = csv.writer(f, dialect=data.DatDialect)
+        w.writerows(results)
--- scripts/make_sensitivity_configs.py
+++ scripts/make_sensitivity_configs.py
@@ -14,7 +14,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-FRACTIONAL_DX = 0.01
+FRACTIONAL_DX = 0.001
 
 FILENAME_TEMPLATE = 'definitions/sensitivity/rho_%s.yaml'
 
