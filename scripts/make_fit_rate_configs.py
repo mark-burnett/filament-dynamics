6724fc3c9df148f9e38bb80887de5095764610bc
Mark Burnett <mark.m.burnett@gmail.com>
2011-10-31 08:53:24 -0500

    Added fit rate creation script.
    Updated fit rates.

--- scripts/make_fit_rate_configs.py
+++ scripts/make_fit_rate_configs.py
@@ -14,45 +14,17 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-FRACTIONAL_DX = 0.001
-
-COOP_FILENAME_TEMPLATE = 'definitions/sensitivity/rho_%s.yaml'
+COOP_FILENAME_TEMPLATE = 'definitions/fit_rates/rho_%i.yaml'
 COOP_FILE_TEMPLATE = \
-"""name: 'Halftime Sensitivity, rho_c = %s'
-
-import:
-    - 'sensitivity/base.yaml'
-    - 'random_cooperative_model.yaml'
-
-global_parameters:
+"""global_parameters:
     release_cooperativity: %s
-
-parameter_specifications:
-    halftime_sensitivity:
-        simulation:
-            release_rate:
-                lower_bound: %s
-                upper_bound: %s
-                mesh_type:   linear
-                num_points:  3
+    release_rate: %s
 """
 
 VEC_FILENAME = 'definitions/sensitivity/vectorial.yaml'
 VEC_FILE_TEMPLATE = \
-"""name: 'Halftime Sensitivity, vectorial'
-
-import:
-    - 'sensitivity/base.yaml'
-    - 'random_vectorial_model.yaml'
-
-parameter_specifications:
-    halftime_sensitivity:
-        simulation:
-            release_rate:
-                lower_bound: %s
-                upper_bound: %s
-                mesh_type:   linear
-                num_points:  3
+"""global_parameters:
+    release_rate: %s
 """
 
 from actin_dynamics.io import data
@@ -64,20 +36,16 @@ def main():
     cooperativities, rates = rate_results[0], rate_results[1]
 
     for rho, r in zip(cooperativities, rates):
-        lower_rate = r * (1 - FRACTIONAL_DX)
-        upper_rate = r * (1 + FRACTIONAL_DX)
         with open(COOP_FILENAME_TEMPLATE % rho, 'w') as f:
-            f.write(COOP_FILE_TEMPLATE % (rho, rho, lower_rate, upper_rate))
+            f.write(COOP_FILE_TEMPLATE % (rho, r))
 
     # Vectorial part
     vec_rates = data.load_data('results/melki_vectorial_rate.dat')
 
     r = vec_rates[1][0]
-    lower_rate = r * (1 - FRACTIONAL_DX)
-    upper_rate = r * (1 + FRACTIONAL_DX)
 
     with open(VEC_FILENAME, 'w') as f:
-        f.write(VEC_FILE_TEMPLATE % (lower_rate, upper_rate))
+        f.write(VEC_FILE_TEMPLATE % r)
 
 
 if '__main__' == __name__:
