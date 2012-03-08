b51440ad98ffd5088288fd11d7ae713c446867a6
Mark Burnett <mark.m.burnett@gmail.com>
2012-03-08 09:50:24 -0600

    Added new mesh types.

--- actin_dynamics/numerical/meshes.py
+++ actin_dynamics/numerical/meshes.py
@@ -41,15 +41,24 @@ def make_mesh(lower_bound, upper_bound, num_points, mesh_type):
     if 'linear' == mesh_type.lower():
         return numpy.linspace(lower_bound, upper_bound, num_points)
     elif 'log' == mesh_type.lower():
-        ulower_bound = math.log(lower_bound)
-        uupper_bound = math.log(upper_bound)
-        transformed_range = numpy.linspace(ulower_bound, uupper_bound,
-                                               num_points)
-        return [math.exp(tr) for tr in transformed_range]
+        return log_mesh(lower_bound, upper_bound, num_points)
     elif 'shiftlog' == mesh_type.lower():
         return shiftlog_mesh(lower_bound, upper_bound, num_points)
+    elif 'splitlog' == mesh_type.lower():
+        return splitlog_mesh(lower_bound, upper_bound, num_points)
+    elif 'tan' == mesh_type.lower():
+        return tan_mesh(lower_bound, upper_bound, num_points)
+    elif 'split' == mesh_type.lower():
+        return split_mesh(lower_bound, upper_bound, num_points)
     else:
         raise RuntimeError('Unsupported mesh type specified for make_mesh.')
+
+def log_mesh(lower_bound, upper_bound, num_points):
+    ulower_bound = math.log(lower_bound)
+    uupper_bound = math.log(upper_bound)
+    transformed_range = numpy.linspace(ulower_bound, uupper_bound,
+                                           num_points)
+    return numpy.exp(transformed_range)
 
 def shiftlog_mesh(lower_bound, upper_bound, num_points):
     '''
@@ -65,6 +74,29 @@ def shiftlog_mesh(lower_bound, upper_bound, num_points):
     logmesh *= upper_bound
 
     return logmesh
+
+def splitlog_mesh(lower_bound, upper_bound, num_points):
+    split_point = float(upper_bound - lower_bound) / 2 + lower_bound
+
+    lower_mesh = log_mesh(lower_bound, split_point, num_points / 2 + 1)
+    upper_mesh = numpy.linspace(split_point, upper_bound, num_points / 2 + 1)
+
+    return numpy.hstack((lower_mesh[:-1], upper_mesh))
+
+def split_mesh(lower_bound, upper_bound, num_points):
+    split_point = float(upper_bound - lower_bound) / 5 + lower_bound
+
+    lower_mesh = numpy.linspace(lower_bound, split_point, num_points / 2 + 1)
+    upper_mesh = numpy.linspace(split_point, upper_bound, num_points / 2 + 1)
+
+    return numpy.hstack((lower_mesh[:-1], upper_mesh))
+
+def tan_mesh(lower_bound, upper_bound, num_points):
+    tlb = numpy.arctan(lower_bound)
+    tub = numpy.arctan(upper_bound)
+
+    transformed_mesh = numpy.linspace(tlb, tub, num_points)
+    return numpy.tan(transformed_mesh)
 
 
 def parameters_from_spec(par_specs):
